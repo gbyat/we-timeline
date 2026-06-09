4348d472a9bb55c877488f2bb07c151c948da032
gbyat <gaby@gby.at>
2026-06-09 22:43:11 +0200

    Update timeline styles and asset versions; enhance padding, background, and shadow effects for improved visual presentation. Adjust asset versioning for both timeline and timeline-item components.

--- build/timeline-item/index.asset.php
+++ build/timeline-item/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '9804a7239e2a2a0f3fb8');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'f26ca1594cc6113dbcf8');
--- build/timeline/index.asset.php
+++ build/timeline/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '7e48c28cb4e8e0d78208');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'ceaa1713ac868b878901');
