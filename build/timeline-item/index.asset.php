30b406e95e407f857e9cd6dd7a0e6e98e1c411d1
gbyat <gaby@gby.at>
2026-07-09 11:30:47 +0200

    Update timeline block icon SVG for improved rendering; adjust icon sizes for better responsiveness. Update asset versions for timeline and timeline-item components.

--- build/timeline-item/index.asset.php
+++ build/timeline-item/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '8a84e7ef0dc0a28718e0');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '55d7aadf6253b60740bf');
--- build/timeline/index.asset.php
+++ build/timeline/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'e71f12a3feb608219982');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '617ac50c08574c13246b');
